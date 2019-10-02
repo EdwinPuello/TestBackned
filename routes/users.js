939aed3eab78703a5bcaeaf59392bd8cde062121
unknown <puello852@gmail.com>
2019-10-02 15:01:42 -0500

    xdd

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/test', function(req, res, next) {
-  res.send('Test user');
+  res.json({
+    re:true
+  })
 });
+
+
 
 module.exports = router;
